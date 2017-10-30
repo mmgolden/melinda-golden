241e08cdc12c4aa940d6351b65b85e890c4f24b8
Melinda Golden <mmgoldenart@gmail.com>
2017-10-29 20:51:59 -0400

    Working contact form
    Working contact form

---
+++ contact.js
@@ -0,0 +1,29 @@
+// Contact form
+$('#contact-form').validator();
+
+
+// When the form is submitted
+$('#contact-form').on('submit', function (e) {
+
+    if (!e.isDefaultPrevented()) {
+        var url = "contact.php";
+
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: $(this).serialize(),
+            success: function (data)
+            { 
+                var messageAlert = 'alert-' + data.type;
+                var messageText = data.message;
+                var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+
+                if (messageAlert && messageText) {
+                    $('#contact-form').find('.messages').html(alertBox);
+                    $('#contact-form')[0].reset();
+                }
+            }
+        });
+        return false;
+    }
+});
