e0ad135d9c03b870464c6c3f72193ab5bba14b05
Melinda Golden <mmgoldenart@gmail.com>
2017-11-04 16:05:18 -0400

    Added case study
    Added case study

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,16 @@ var scroll = new SmoothScroll('a[href*="#"]');
 // Current year
 var currentYear = new Date().getFullYear();
 document.getElementById("current-year").textContent = currentYear;
+
+// Hide mobile menu on click
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
+
+// Slick
+$('.slick').slick({
+    dots: true, 
+    infinite: true
+});
